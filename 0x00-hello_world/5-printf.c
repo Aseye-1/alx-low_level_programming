82c5a79db022c4036f966050a169cdce315a19cc
Aseye-1 <aboraseye@gmail.com>
2022-12-09 03:30:59 -0800

    hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-*hello_printf - The use of printf function
+*main - The use of printf function
 *
-*Return: Always 0
+*Return: Always 0 (success)
  */
 
-int hello_printf(void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
