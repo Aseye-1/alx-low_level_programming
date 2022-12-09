610d89370ce1fa58ad5fdfd515668ad185ebb367
Aseye-1 <aboraseye@gmail.com>
2022-12-08 17:49:48 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always
+ */
+
+int main(void)
+{
+
+     puts("\"Programming is like building a multi lingual puzzle);
+     return (0);
+}
