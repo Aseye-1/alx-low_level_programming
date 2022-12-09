81a6d26e714de74fd389aaf50d64f435ab847889
Aseye-1 <aboraseye@gmail.com>
2022-12-09 02:55:47 -0800

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+
 /**
-*main - uses the puts function
-*prints exactly and followed by a new line
-*Return : Always 0 if no errors otherwise return non-zero value. 
-*/
+* main - serves as entry point for the program
+*
+* Return : Always 0 (success)
+ */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
