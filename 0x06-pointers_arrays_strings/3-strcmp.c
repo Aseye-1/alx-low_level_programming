cb1fb571d54b4f4592ac818ed78ab721c2a6fbb7
Aseye-1 <aboraseye@gmail.com>
2022-12-22 12:15:16 -0800

    compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_strcmp - string to compare
+*@s1: first string
+*@s2: second string
+*Return: value of string
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[a] - s2[a] == 0 && s1[a] != '\0')
+	{
+		a++;
+	}
+	return (s1[b] - s2[b]);
+}
