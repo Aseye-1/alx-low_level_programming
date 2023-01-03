8e651e8b0ebb2dbc01d0dc357ffa8f0d4a94eff4
Aseye-1 <aboraseye@gmail.com>
2023-01-03 12:37:48 -0800

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,10 +6,10 @@
 *@s: A pointer to the memory area to be filled.
 *@c: The character to fill the memory area with.
 *@n: The number of bytes to be filled.
-*description _memset: over there 
+*description _memset: over there
 *Return: A pointer to the filled memory area @s.
  */
-char *_memset(char *s, char b, unsigned int n);
+void *_memset(void *s, int c, size_t n);
 {
 	unsigned int index;
 	unsigned char *memory = s, value = c;
