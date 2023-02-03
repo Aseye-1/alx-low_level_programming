aa3c2b335519b147343ce48662866a0e1579686b
Aseye-1 <aboraseye@gmail.com>
2023-02-03 05:00:08 +0000

    Task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,13 +10,9 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	int bit;
 
-	if (index >> 31)
+	if (index > 64)
 		return (-1);
-	bit = 1 << index;
-
-	*n = *n | bit;
-
+	*n += 1 << index;
 	return (1);
 }
